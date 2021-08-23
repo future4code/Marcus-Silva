75ae99267a61e6d880005e823efe5437f56d4491
marcusluis <marcusluis.mail@gmail.com>
2021-08-23 18:02:45 -0300

    Exercicio 6

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -37,7 +37,7 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  // return Math.max(...array);
+  return Math.max(...array);
 }
 
 // EXERCÍCIO 07
