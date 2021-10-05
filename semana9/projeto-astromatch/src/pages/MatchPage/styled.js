1ec50fdeeec66de2b82a0fcf0846f7264e703897
marcusluis <marcusluis.mail@gmail.com>
2021-10-05 01:07:19 -0300

    add react-icons e estilizei a HomePage

---
+++ semana9/projeto-astromatch/src/pages/MatchPage/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Profiles = styled.div`
+  border: 2px solid #bdbdbd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 25px;
+  padding: 25px;
+  width: 300px;
+`;
