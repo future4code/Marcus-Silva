b7936ee0ad1f3099955abb15e6d2c8a8ec31b72b
marcusluis <marcusluis.mail@gmail.com>
2021-10-29 15:22:03 -0300

    test

--- semana12/projeto-labeddit/src/pages/PostsPage/PostsPage.js
+++ semana12/projeto-labeddit/src/pages/PostsPage/PostsPage.js
@@ -1,44 +1,12 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useHistory, useParams } from "react-router";
-import { BASE_URL } from "../../constants/urls";
 import useProtectedPage from "../../hooks/useProtectedPage";
-import { Card } from "../FeedPage/styled";
 
 const PostPage = () => {
   useProtectedPage();
 
-  const [postDetails, setPostDetails] = useState(null);
-  const params = useParams();
-  const history = useHistory();
-
-  useEffect(() => {
-    if (!params.id) {
-      history.push("/");
-    }
-  });
-
-  useEffect(() => {
-    const getPostComments = {
-      headers: {
-        Authorization: localStorage.getItem("token"),
-      },
-    };
-
-    axios
-      .get(`${BASE_URL}/posts/:id/comments`)
-      .then((res) => {
-        console.log(res.data.post);
-      })
-      .catch((err) => {
-        console.error(err.response);
-      });
-  });
-
   return (
     <div>
       <p>PostPage</p>
-      {postDetails !== null && <Card post={postDetails} />}
     </div>
   );
 };
