60ca83f1eb2f8fd6d554d518d4485af5b6ee9325
marcusluis <marcusluis.mail@gmail.com>
2022-01-20 16:13:40 -0300

    importando pag pagination

--- semana22/src/App.jsx
+++ semana22/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Pagination from "./components/Pagination/Pagination";
 import GlobalStyle from "./GlobalStyle";
 import Home from "./pages/Home/Home";
 
@@ -9,6 +10,7 @@ const App = () => {
             <GlobalStyle />
             <Header />
             <Home />
+            <Pagination />
         </div>
     );
 };
