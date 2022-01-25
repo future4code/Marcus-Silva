b526e5061c5ad98a413956927082f395d42d6577
marcusluis <marcusluis.mail@gmail.com>
2022-01-25 15:09:11 -0300

    paginacao funcionando

--- semana22/src/App.jsx
+++ semana22/src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
             <GlobalStyle />
             <Header />
             <Home />
-            <Pagination />
         </div>
     );
 };
--- semana22/src/pages/Home/Home.jsx
+++ semana22/src/pages/Home/Home.jsx
@@ -2,39 +2,47 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Card, CardContainer, MovieTitle } from "./styled";
 import { API_KEY, BASE_URL, IMG_URL } from "../../constants/urls";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(0);
+
+    console.log(page);
 
     useEffect(() => {
         axios
-            .get(`${BASE_URL}/movie/popular${API_KEY}`)
+            .get(`${BASE_URL}/movie/popular${API_KEY}&page=${page + 1}`)
             .then((response) => {
-                console.log(response.data);
                 setMovies(response.data.results);
             })
             .catch((error) => {
-                console.error(error.message);
+                console.log(error.message);
+                alert("Um erro inesperado aconteceu.");
             });
-    }, []);
+    }, [page]);
 
     return (
-        <CardContainer>
-            {movies &&
-                movies.map((movie, index) => {
-                    return (
-                        <Card key={index}>
-                            <img
-                                src={IMG_URL + movie.poster_path}
-                                alt={movie.title}
-                            />
+        <>
+            <CardContainer>
+                {movies &&
+                    movies.map((movie, index) => {
+                        return (
+                            <Card key={index}>
+                                <img
+                                    src={IMG_URL + movie.poster_path}
+                                    alt={movie.title}
+                                />
 
-                            <MovieTitle>{movie.title}</MovieTitle>
-                            <p>{movie.release_date}</p>
-                        </Card>
-                    );
-                })}
-        </CardContainer>
+                                <MovieTitle>{movie.title}</MovieTitle>
+                                <p>{movie.release_date}</p>
+                            </Card>
+                        );
+                    })}
+            </CardContainer>
+
+            <Pagination setPage={setPage} />
+        </>
     );
 };
 
