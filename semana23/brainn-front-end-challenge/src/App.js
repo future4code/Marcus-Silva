fb9c63c8cf667568581ef3afbca7f6bc22af4f34
marcusluis <marcusluis.mail@gmail.com>
2022-01-28 16:53:02 -0300

    iniciando case brainn frontend challenge

---
+++ semana23/brainn-front-end-challenge/src/App.js
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+import GlobalStyle from "./styles/GlobalStyle";
+
+const App = () => {
+    return (
+        <div>
+            <GlobalStyle />
+            <Sidebar />
+        </div>
+    );
+};
+
+export default App;
