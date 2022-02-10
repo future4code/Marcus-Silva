2fb481272303e4fda1e0168e8f631c3776c4310a
marcusluis <marcusluis.mail@gmail.com>
2022-02-10 17:25:32 -0300

    commit

--- semana25/amaro-back-end-challenge/src/endpoints/products.ts
+++ semana25/amaro-back-end-challenge/src/endpoints/products.ts
@@ -1,1 +1,21 @@
 import { Request, Response } from "express";
+import { connection } from "../data/BaseDatabase";
+import { generateId } from "../services/IdGenerator";
+
+export async function products(req: Request, res: Response) {
+    try {
+        const { name, tags } = req.body;
+
+        if (!name || !tags) {
+            res.status(422).send("Por favor, insira todas as informações.");
+        }
+
+        const id = generateId();
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(400).json({ message: error.message });
+        } else {
+            res.status(400).send({ message: "Um erro inesperado aconteceu." });
+        }
+    }
+}
